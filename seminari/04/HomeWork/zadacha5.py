b6c3a1b8e9780ad4ceaa9dab403230bb4c17c995
hitman111 <ganibal1993@gmail.com>
2022-12-31 11:49:13 +0500

    ДЗ 4(2)

---
+++ seminari/04/HomeWork/zadacha5.py
@@ -0,0 +1,25 @@
+def otkr(a):
+    data=open(f'{a}','r')   
+    result=data.read()
+    data.close
+    return result
+
+def razl(st):
+    st=st.replace('=0','')
+    rezult=st.split('+')
+    kof=[0,0,0]
+    for i in range(len(rezult)):
+        if 'x^2' in rezult[i]:
+           rezult[i]=rezult[i].replace('x^2','')
+           kof[0]=int(rezult[i])
+        elif 'x^1' in rezult[i]:
+           rezult[i]=rezult[i].replace('x^1','')
+           kof[1]=int(rezult[i])
+        else:           
+           kof[2]=int(rezult[i])
+    return kof
+      
+a=razl(otkr('1.txt'))
+b=razl(otkr('2.txt'))
+c=list(map(sum, zip(a,b)))
+print(f'{c[0]}x^2+{c[1]}x^1+{c[2]}=0')
